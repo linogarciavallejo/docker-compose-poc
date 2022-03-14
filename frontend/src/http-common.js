fa4cb31acd7686c9b5d8b7984df1fca4f462ff49
Lino Garcia <lgarciavallejo@paypal.com>
2022-03-13 19:33:55 -0600

    Frontend dockerizado, aplicación 100% funcional

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://localhost:6868/api",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:6868/api",
   headers: {
     "Content-type": "application/json",
   },
